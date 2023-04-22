69383d58dd2f990150ae45d1884126d8e908a7b7
alihussein81 <77223681+alihussein81@users.noreply.github.com>
2023-04-23 01:12:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 setInterval(function() {
     var image = document.getElementById('myImage');
     var timestamp = new Date().getTime();
-    image.src = "https://drive.google.com/uc?export=view&id=1-UDwRUB7rhsgYXenpIu18kjqmJpbIy5i?time=" + timestamp;
+    image.src = "https://drive.google.com/file/d/1-e24K9vZs_6Wm0ckWQwElA6mVFVOc43R/view?usp=sharing" + timestamp;
 }, 20000);
